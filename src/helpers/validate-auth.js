c973bfaafb9764f3f03b7ad3938c08fba559e371
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2023-12-31 19:02:16 -0500

    Ya no se

--- src/helpers/validate-auth.js
+++ src/helpers/validate-auth.js
@@ -1,16 +1,15 @@
-
-
 // Metodo para proteger rutas y a la vez esta siendo exportada
 module.exports.isAuthenticated = (req,res,next)=>{
-    // Si existe un inicio de sesion 
+    // Si existe un inicio de sesion
     if(req.isAuthenticated()){
-        // continuar
+        // Continuar
         return next()
     }
     // Redireccionamiento
     res.redirect('/user/login')
 }
 
+// Redireccionar a la vista portafolios si el usuario ya inicio sesion
 module.exports.redirectIfAuthenticated = (req, res, next)=>{
     if (req.isAuthenticated()) {
         return res.redirect('/portafolios');
